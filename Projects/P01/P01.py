a458b6b52e9a8c79e8fba2d5dc812a2b358720d6
Andre Fonseca <afbfonseca@gmail.com>
2018-03-07 21:55:19 +0000

    Exercise 3 and 4

--- Projects/P01/P01.py
+++ Projects/P01/P01.py
@@ -6,10 +6,11 @@ from lib import image_metrics as metrics
 
 def main():
     img = cv2.imread('resources/lenac.tif')
+    path = 'resources/lenac.tif'
     # ex1(img)
-    ex2(img)
-    ex3()
-    ex4()
+    ex2(img, path)
+    ex3(img, path)
+    ex4(img)
     ex5()
     ex6()
     ex7()
@@ -27,24 +28,24 @@ def ex1(img):
     cv2.destroyAllWindows()
 
 
-def ex2(img):
-    path1 = 'P01/resources/file1.jpg'
-    path2 = 'P01/resources/file2.jpg'
+def ex2(img, path):
+    path_80 = 'P01/resources/img_80.jpg'
+    path_100 = 'P01/resources/img_100.jpg'
 
-    cv2.imwrite(path1, img, (cv2.IMWRITE_JPEG_QUALITY, 80))
-    cv2.imwrite(path2, img, (cv2.IMWRITE_JPEG_QUALITY, 100))
+    cv2.imwrite(path_80, img, (cv2.IMWRITE_JPEG_QUALITY, 80))
+    cv2.imwrite(path_100, img, (cv2.IMWRITE_JPEG_QUALITY, 100))
 
-    img_80 = cv2.imread(path1)
-    img_100 = cv2.imread(path2)
+    img_80 = cv2.imread(path_80)
+    img_100 = cv2.imread(path_100)
 
-    compression_rate = metrics.compression_rate('resources/lenac.tif', path1)
+    compression_rate = metrics.compression_rate(path, path_80)[0]
 
     print('# JPEG Quality 80')
     print('Compression rate: {}'.format(compression_rate))
     print('SNR: {}'.format(metrics.snr(img, img_80)))
     print('PSNR: {}\n'.format(metrics.psnr(img, img_80)))
 
-    compression_rate = metrics.compression_rate('resources/lenac.tif', path2)
+    compression_rate = metrics.compression_rate(path, path_100)[0]
 
     print('# JPEG Quality 100')
     print('Compression rate: {}'.format(compression_rate))
@@ -52,12 +53,24 @@ def ex2(img):
     print('PSNR: {}\n'.format(metrics.psnr(img, img_100)))
 
 
-def ex3():
-    pass
+def ex3(img, path):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    path_gray = 'P01/resources/img_gray.jpg'
+
+    cv2.imwrite(path_gray, img_gray)
+
+    compression_rate = metrics.compression_rate(path, path_gray)
+
+    print('# Gray image')
+    print('Compression rate: {}'.format(compression_rate[0]))
+    print('Original image size: {} bytes'.format(compression_rate[1]))
+    print('Gray image size: {} bytes'.format(compression_rate[2]))
 
 
-def ex4():
-    pass
+def ex4(img):
+    plt.hist(img.ravel(), 256, [0, 256])
+    plt.title('Exercise 4 histogram')
+    plt.show()
 
 
 def ex5():
