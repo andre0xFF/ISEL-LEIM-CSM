41fef7eed5d9ef17392e7ba5724ea0ee220233a9
tiagodoliveira <33941040+tiagodoliveira@users.noreply.github.com>
2018-05-19 18:24:08 +0100

    update jpeg

--- Projects/P03/src/jpeg.py
+++ Projects/P03/src/jpeg.py
@@ -8,19 +8,27 @@ import numpy as np
 
 # Encodes a raw image
 # Returns: Encoded images and number of blocks per line
-def encode(raw_image: np.array) -> (np.array, np.int):
+def encode(raw_image: np.array, factor: float) -> (np.array, np.int):
     sampled_image = apply_subsample(raw_image, 4, 2, 0)
     ycc_image = ycc.encode(sampled_image)
     blocks = make_blocks(ycc_image)
+    dc_elements = np.zeros(len(blocks))
+    previous_quantified_block = None
 
-    for block in blocks:
-        block_dct = dct.encode(block)
-        block_quantification = quantification.encode()
+    for i in range(len(blocks)):
 
-        block_dc = dc.encode(block_quantification)
+        block_dct = dct.encode(blocks[i])
+        block_quantification = quantification.encode(block_dct, factor)
+
+        if i == 0:
+            dc_elements[i] = dc.encode(None, block_quantification)
+        else:
+            dc_elements[i] = dc.encode(previous_quantified_block, block_quantification)
+
         block_ac = ac.encode(block_quantification)
+        previous_quantified_block = block_quantification.copy()
 
-        entropic_encoding(block_dc, block_ac)
+        entropic_encoding(dc_elements, block_ac)
 
     return None
 
@@ -35,11 +43,43 @@ def apply_subsample(image: np.array, sample_size: int, horizontal_ratio: int, ve
     pass
 
 
-def make_blocks(image: np.array) -> (np.array, np.int):
-    # TODO: Transform the image in multiple of 8
-    # TODO: Slice the image into multiple bocks of 8x8
-    pass
+def __apply_image_croping(image, mod_factor):
+    line_dim, col_dim = image.shape
 
+    paddingLines = line_dim % mod_factor
+    paddingCols = col_dim % mod_factor
+
+    for i in range(paddingLines):
+        image = np.delete(image, line_dim - 1, 0)
+        line_dim -= 1
+
+    for i in range(paddingCols):
+        image = np.delete(image, col_dim - 1, 1)
+        col_dim -= 1
+
+    return image
+
+
+def make_blocks(image: np.array) -> (np.array, np.int):
+
+    m_size = 8
+    image = __apply_image_croping(image, m_size) * 1.0
+
+    line_dim, col_dim = image.shape
+    number_of_blocks = int(line_dim / m_size) * int(col_dim / m_size)
+    blocks = np.zeros((number_of_blocks, 8, 8))
+    i = 0
+
+    for line in range(int(line_dim / m_size)):
+        for col in range(int(col_dim / m_size)):
+            blocks[i] = __get_submatrix(image, line * m_size, col * m_size, m_size)
+            i += 1
+
+    return blocks
+
+
+def __get_submatrix(matrix, line, col, dim):
+    return matrix[line: line + dim, col: col + dim]
 
 def entropic_encoding(block_dc: np.array, block_ac: np.array) -> np.array:
     # Merges DC with AC and append's eob
