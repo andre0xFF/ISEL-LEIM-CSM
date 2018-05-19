f5d08bac47ae0a555848f9f3e1d4bb56e2ec6bb3
Andre Fonseca <afbfonseca@gmail.com>
2018-05-19 17:14:13 +0100

    Minor tweaks

--- Projects/P03/src/dct.py
+++ Projects/P03/src/dct.py
@@ -1,11 +1,11 @@
 import numpy as np
 from cv2 import dct
 from cv2 import idct
-from cv2 import imread
-from cv2 import imwrite
 
-def encode(block: np.array)  -> np.array:
+
+def encode(block: np.array) -> np.array:
     return dct(block)
 
-def decode(block: np.array)  -> np.array:
+
+def decode(block: np.array) -> np.array:
     return idct(block)
